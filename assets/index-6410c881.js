6f7af6d3b78adbb78b89092aa09f14f2ca835cbb
Youting <boy80314@gmail.com>
2023-09-07 13:28:29 +0800

    Updates

---
+++ assets/index-6410c881.js
@@ -0,0 +1,1 @@
+import"./bootstrap.min-41ee4726.js";new Swiper(".ai-swiper",{mousewheel:!0,slidesPerView:"3",spaceBetween:24,scrollbar:{el:".swiper-scrollbar",draggable:!0}});new Swiper(".what-section-swiper",{autoplay:{delay:4e3},navigation:{nextEl:".sw-right",prevEl:".sw-left"}});
